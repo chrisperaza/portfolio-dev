f2fa4e44ecccfa61c7585fdffc218e3474b870ba
Christian Peraza <christianperaza2000@gmail.com>
2026-07-29 00:03:24 -0300

    Create main component

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+// Import components
+import Banner from './Banner';
+
+const Main = () => {
+  return (
+    <main>
+      <Banner />
+    </main>
+  );
+};
+
+export default Main;
