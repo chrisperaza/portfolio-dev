e9da3b8cb4e3bf1183e1ffb3e2639ccd0267da81
Christian Peraza <christianperaza2000@gmail.com>
2026-07-29 00:03:42 -0300

    Import main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 // import React from 'react'
 // Import components
 import Header from './components/Header';
+import Main from './components/Main';
 
 const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 };
 
 export default App;
