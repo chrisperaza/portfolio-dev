4dbe07fcaa02d80d7043ba33deba95d55695926e
Christian Peraza <christianperaza2000@gmail.com>
2026-07-29 23:38:22 -0300

    Add social media database

---
+++ src/database/socialMediaArray.js
@@ -0,0 +1,17 @@
+export const socialMediaArray = [
+  {
+    id: 1,
+    name: 'GitHub',
+    link: 'https://github.com/chrisperaza',
+  },
+  {
+    id: 2,
+    name: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/chrisperaza',
+  },
+  {
+    id: 3,
+    name: 'Instagram',
+    link: 'https://www.instagram.com/chrisperaza13/',
+  },
+];
