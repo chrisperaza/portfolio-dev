4dcc528074a946674aae79ea6d9d38667e89eb8c
Christian Peraza <christianperaza2000@gmail.com>
2026-07-29 23:39:52 -0300

    Import about me section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,12 +2,14 @@
 // Import components
 import Banner from './Banner';
 import SkillsSection from './SkillsSection';
+import AboutMeSection from './AboutMeSection';
 
 const Main = () => {
   return (
     <main className='main'>
       <Banner />
       <SkillsSection />
+      <AboutMeSection />
     </main>
   );
 };
