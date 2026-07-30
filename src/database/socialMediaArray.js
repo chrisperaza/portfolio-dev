e57f79e97820635abf0cbf3714fb12183a142780
Christian Peraza <christianperaza2000@gmail.com>
2026-07-30 14:19:12 -0300

    Add email object and titles

--- src/database/socialMediaArray.js
+++ src/database/socialMediaArray.js
@@ -3,15 +3,24 @@ export const socialMediaArray = [
     id: 1,
     name: 'GitHub',
     link: 'https://github.com/chrisperaza',
+    title: 'Explore my code & projects',
   },
   {
     id: 2,
     name: 'LinkedIn',
     link: 'https://www.linkedin.com/in/chrisperaza',
+    title: 'Professional network & experience',
   },
   {
     id: 3,
     name: 'Instagram',
     link: 'https://www.instagram.com/chrisperaza13/',
+    title: 'Beyond the code',
+  },
+  {
+    id: 4,
+    name: 'Gmail',
+    link: 'mailto:christianperaza2000@gmail.com',
+    title: 'Get in touch for collabs',
   },
 ];
