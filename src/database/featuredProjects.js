03fde0dc3941fde828c03cf7602a5b67c5a87463
Christian Peraza <christianperaza2000@gmail.com>
2026-08-01 22:54:36 -0300

    Add all featured projects and change name

---
+++ src/database/featuredProjects.js
@@ -0,0 +1,82 @@
+export const featuredProjectsArray = [
+  {
+    id: 1,
+    image:
+      'https://res.cloudinary.com/dx29auvvm/image/upload/v1785635411/spotify-clone-portfolio-cover_igpz1j.png',
+    title: 'Spotify Clone - MERN Stack App',
+    techs: [
+      {
+        name: 'React',
+        color: '#2A4663',
+      },
+      {
+        name: 'Node',
+        color: '#245121',
+      },
+      {
+        name: 'Express',
+        color: '#38495A',
+      },
+      {
+        name: 'MongoDB',
+        color: '#245121',
+      },
+    ],
+    description:
+      'Full-stack online music player. Features audio streaming and a responsive design adapted for desktop and mobile.',
+    repoLink: 'https://github.com/chrisperaza/spotify-clone',
+    demoLink: 'https://spotify-clone-hllg.onrender.com/',
+  },
+  {
+    id: 2,
+    image:
+      'https://res.cloudinary.com/dx29auvvm/image/upload/v1785635420/movie-finder-portfolio-cover_xnkxh0.png',
+    title: 'Movie Finder - JavaScript Vanilla App',
+    techs: [
+      {
+        name: 'HTML5',
+        color: '#61632A',
+      },
+      {
+        name: 'CSS3',
+        color: '#2A4663',
+      },
+      {
+        name: 'JavaScript',
+        color: '#61632A',
+      },
+      {
+        name: 'Rest API',
+        color: '#38495A',
+      },
+    ],
+    description:
+      'Interactive web catalog for exploring movies and viewing their descriptions, release dates, and ratings in real-time. Built without frameworks to demonstrate mastery of web development fundamentals, DOM manipulation, and REST API consumption.',
+    repoLink: 'https://github.com/chrisperaza/movie_app_wdd330_final_project',
+    demoLink: 'https://movie-app-wdd330-final-project.netlify.app/',
+  },
+  {
+    id: 3,
+    image:
+      'https://res.cloudinary.com/dx29auvvm/image/upload/v1785635411/image-gallery-portfolio-cover_focl4l.png',
+    title: 'Image Gallery - React App',
+    techs: [
+      {
+        name: 'React',
+        color: '#2A4663',
+      },
+      {
+        name: 'JavaScript',
+        color: '#61632A',
+      },
+      {
+        name: 'Rest API',
+        color: '#38495A',
+      },
+    ],
+    description:
+      'Image gallery that connects to an external API to search for and display high-quality images based on keywords. It features a responsive interface, loading states, and an optimized display.',
+    repoLink: 'https://github.com/chrisperaza/image-gallery',
+    demoLink: 'https://image-gallery-challenge.netlify.app/',
+  },
+];
