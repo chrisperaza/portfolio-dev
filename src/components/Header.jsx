b662bb7e406285d0056dbeef0e437d1d153b4620
Christian Peraza <christianperaza2000@gmail.com>
2026-08-05 16:22:56 -0300

    Import nav component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,24 @@ import christianPhoto from '../assets/christianPeraza_photo.png';
 // Import icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+// Import components
+import Navigation from './Navigation';
 
 const Header = () => {
   return (
     <header className='header'>
-      <img
-        className='profilePhoto-small'
-        src={christianPhoto}
-        alt='Profile photo of Christian Peraza smiling, wearing a white shirt and dark green tie, with a blurred nature background.'
-      />
-      <FontAwesomeIcon className='burgerMenu-icon fa-lg' icon={faBars} />
+      <section>
+        <div>
+          <img
+            className='profilePhoto-small'
+            src={christianPhoto}
+            alt='Profile photo of Christian Peraza smiling, wearing a white shirt and dark green tie, with a blurred nature background.'
+          />
+          <p>Christian Peraza</p>
+        </div>
+        <Navigation />
+        <FontAwesomeIcon className='burgerMenu-icon fa-lg' icon={faBars} />
+      </section>
     </header>
   );
 };
