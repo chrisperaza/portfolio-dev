82291e760e82d1711ef315154809b69bd350909e
Christian Peraza <christianperaza2000@gmail.com>
2026-08-05 01:19:47 -0300

    Add other projects section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,6 +7,7 @@ import AboutMeSection from './AboutMeSection';
 import ContactMeSection from './ContactMeSection';
 // Import database
 import { featuredProjectsArray } from '../database/featuredProjects';
+import { otherProjectsArray } from '../database/otherProjectsArray';
 
 const Main = () => {
   return (
@@ -17,6 +18,12 @@ const Main = () => {
         description='Take a look at some of the projects I have designed and developed. These include original ones, some clones, and landing pages, among others.'
         itemsQuantity={3}
         database={featuredProjectsArray}
+      />
+      <ProjectsSection
+        title='Other projects'
+        description='Discover how creative I can be.'
+        itemsQuantity={6}
+        database={otherProjectsArray}
       />
       <SkillsSection />
       <AboutMeSection />
