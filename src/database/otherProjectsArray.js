5b0b20cd2786855082b1cfe041386075457d648e
Christian Peraza <christianperaza2000@gmail.com>
2026-08-05 01:19:10 -0300

    Create sample data projects

---
+++ src/database/otherProjectsArray.js
@@ -0,0 +1,98 @@
+export const otherProjectsArray = [
+  {
+    id: 1,
+    image:
+      'https://res.cloudinary.com/dx29auvvm/image/upload/v1785899152/dump-cover-portfolio_uiy8kt.png',
+    title: 'Project title',
+    techs: [
+      {
+        name: 'React',
+        color: '#2A4663',
+      },
+    ],
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+    repoLink: '#',
+    demoLink: '#',
+  },
+  {
+    id: 2,
+    image:
+      'https://res.cloudinary.com/dx29auvvm/image/upload/v1785899152/dump-cover-portfolio_uiy8kt.png',
+    title: 'Project title',
+    techs: [
+      {
+        name: 'React',
+        color: '#2A4663',
+      },
+    ],
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+    repoLink: '#',
+    demoLink: '#',
+  },
+  {
+    id: 3,
+    image:
+      'https://res.cloudinary.com/dx29auvvm/image/upload/v1785899152/dump-cover-portfolio_uiy8kt.png',
+    title: 'Project title',
+    techs: [
+      {
+        name: 'React',
+        color: '#2A4663',
+      },
+    ],
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+    repoLink: '#',
+    demoLink: '#',
+  },
+  {
+    id: 4,
+    image:
+      'https://res.cloudinary.com/dx29auvvm/image/upload/v1785899152/dump-cover-portfolio_uiy8kt.png',
+    title: 'Project title',
+    techs: [
+      {
+        name: 'React',
+        color: '#2A4663',
+      },
+    ],
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+    repoLink: '#',
+    demoLink: '#',
+  },
+  {
+    id: 5,
+    image:
+      'https://res.cloudinary.com/dx29auvvm/image/upload/v1785899152/dump-cover-portfolio_uiy8kt.png',
+    title: 'Project title',
+    techs: [
+      {
+        name: 'React',
+        color: '#2A4663',
+      },
+    ],
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+    repoLink: '#',
+    demoLink: '#',
+  },
+  {
+    id: 6,
+    image:
+      'https://res.cloudinary.com/dx29auvvm/image/upload/v1785899152/dump-cover-portfolio_uiy8kt.png',
+    title: 'Project title',
+    techs: [
+      {
+        name: 'React',
+        color: '#2A4663',
+      },
+    ],
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+    repoLink: '#',
+    demoLink: '#',
+  },
+];
