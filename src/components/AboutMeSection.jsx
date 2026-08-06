31a265c4115463f46ce2cd40b98e076ef53442dc
Christian Peraza <christianperaza2000@gmail.com>
2026-08-05 23:23:52 -0300

    Add about me container and change title

--- src/components/AboutMeSection.jsx
+++ src/components/AboutMeSection.jsx
@@ -14,23 +14,26 @@ const AboutMeSection = () => {
           alt='Profile photo of Christian Peraza smiling, wearing a white shirt and dark green tie, with a blurred nature background.'
           width={120}
         />
-        <div className='aboutMe-infoSection-titlesContainer'>
-          <div>
-            <h3>Christian Peraza</h3>
-            <p>Associate of Applied Science in Software Development</p>
+        <div>
+          <div className='aboutMe-infoSection-titlesContainer'>
+            <div>
+              <h3>About me</h3>
+              <p>Associate of Applied Science in Software Development</p>
+            </div>
           </div>
+          <p className='aboutMe-infoSection-text'>
+            Currently, I am a student at BYU-Pathway Worldwide pursuing a
+            Bachelor Degree in Software Development. I am passionate about
+            technology and design, which is why I want to become a web
+            developer. <br /> <br /> My goal is to design and develop web
+            applications that provide exceptional user experiences and
+            facilitate or support specific tasks.
+          </p>
+          <SocialMediaList />
+          <button className='aboutMe-infoSection-resumeButton'>
+            <p>Download Resume</p>
+          </button>
         </div>
-        <p className='aboutMe-infoSection-text'>
-          Currently, I am a student at BYU-Pathway Worldwide pursuing a Bachelor
-          Degree in Software Development. I am passionate about technology and
-          design, which is why I want to become a web developer. <br /> <br />{' '}
-          My goal is to design and develop web applications that provide
-          exceptional user experiences and facilitate or support specific tasks.
-        </p>
-        <SocialMediaList />
-        <button className='aboutMe-infoSection-resumeButton'>
-          <p>Download Resume</p>
-        </button>
       </section>
     </section>
   );
