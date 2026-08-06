b2c50c19f2c3983a3bef75a2a82579b935dfa59c
Christian Peraza <christianperaza2000@gmail.com>
2026-08-05 21:23:33 -0300

    Add project section container

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -5,11 +5,13 @@ import ProjectsList from './ProjectsList';
 const ProjectsSection = ({ title, description, itemsQuantity, database }) => {
   return (
     <section className='projectsSection'>
-      <div className='projectsSection-titles'>
-        <h2>{title}</h2>
-        <p>{description}</p>
+      <div>
+        <div className='projectsSection-titles'>
+          <h2>{title}</h2>
+          <p>{description}</p>
+        </div>
+        <ProjectsList itemsQuantity={itemsQuantity} database={database} />
       </div>
-      <ProjectsList itemsQuantity={itemsQuantity} database={database} />
     </section>
   );
 };
