084b266a8f2f4f3e5bbbacd422866096a70e82df
Christian Peraza <christianperaza2000@gmail.com>
2026-08-06 15:48:11 -0300

    Change api url

--- front_end/api/api.js
+++ front_end/api/api.js
@@ -1,10 +1,8 @@
 // Dependencies
 import axios from 'axios';
 
-const NODE_ENV = import.meta.env.MODE;
-
 // API url
-const URL = NODE_ENV === 'development' ? 'http://localhost:3000/api' : '/api';
+const URL = 'https://portfolio-dev-l5fq.onrender.com/api';
 
 // Response
 const featuredProjectsResponse = await axios.get(`${URL}/featuredProjects`);
