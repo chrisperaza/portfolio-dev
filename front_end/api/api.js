902042a75173972a9965d9a927f08398b8add244
Christian Peraza <christianperaza2000@gmail.com>
2026-08-06 14:40:54 -0300

    Create api to connect to backend

---
+++ front_end/api/api.js
@@ -0,0 +1,17 @@
+// Dependencies
+import axios from 'axios';
+
+// API url
+const URL = 'http://localhost:3000';
+
+// Response
+const featuredProjectsResponse = await axios.get(`${URL}/featuredProjects`);
+const otherProjectsResponse = await axios.get(`${URL}/otherProjects`);
+const skillsResponse = await axios.get(`${URL}/skills`);
+const socialMediaResponse = await axios.get(`${URL}/socialMedia`);
+
+// Export responses data
+export const featuredProjectsArray = featuredProjectsResponse.data;
+export const otherProjectsArray = otherProjectsResponse.data;
+export const skillsArray = skillsResponse.data;
+export const socialMediaArray = socialMediaResponse.data;
