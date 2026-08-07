0a062dadee857b598fcbec1f79af1908a86df26c
Christian Peraza <christianperaza2000@gmail.com>
2026-08-07 01:15:17 -0300

    Delete no common sections

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,10 +1,6 @@
 // import React from 'react'
 // Components
 import Header from './components/Header';
-import CheckMyGitHub from './components/CheckMyGitHub';
-import SkillsSection from './components/SkillsSection';
-import AboutMeSection from './components/AboutMeSection';
-import ContactMeSection from './components/ContactMeSection';
 import Footer from './components/Footer';
 // Pages
 import Home from './pages/Home';
@@ -20,10 +16,6 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/projects' element={<Projects />} />
       </Routes>
-      <CheckMyGitHub />
-      <SkillsSection />
-      <AboutMeSection />
-      <ContactMeSection />
       <Footer />
     </BrowserRouter>
   );
