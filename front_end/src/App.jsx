9f83f073af176efa35fb4b70c24986b57f19511e
Christian Peraza <christianperaza2000@gmail.com>
2026-08-06 23:41:18 -0300

    Add pages routes

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,16 +1,31 @@
 // import React from 'react'
-// Import components
+// Components
 import Header from './components/Header';
-import Main from './components/Main';
+import CheckMyGitHub from './components/CheckMyGitHub';
+import SkillsSection from './components/SkillsSection';
+import AboutMeSection from './components/AboutMeSection';
+import ContactMeSection from './components/ContactMeSection';
 import Footer from './components/Footer';
+// Pages
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+// Router dom
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Main />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+      <CheckMyGitHub />
+      <SkillsSection />
+      <AboutMeSection />
+      <ContactMeSection />
       <Footer />
-    </>
+    </BrowserRouter>
   );
 };
 
