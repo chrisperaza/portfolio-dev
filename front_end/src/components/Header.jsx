949a20b799b2e742cfa4fc05550f51aaa7d0b939
Christian Peraza <christianperaza2000@gmail.com>
2026-08-06 23:42:22 -0300

    Add link to logo

--- front_end/src/components/Header.jsx
+++ front_end/src/components/Header.jsx
@@ -1,24 +1,26 @@
 // import React from 'react'
-// Import images
+// Images
 import christianPhoto from '../assets/christianPeraza_photo.png';
-// Import icons
+// Icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-// Import components
+// Components
 import Navigation from './Navigation';
+// React router
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className='header'>
       <section>
-        <div>
+        <Link to={'/'} title='Home'>
           <img
             className='profilePhoto-small'
             src={christianPhoto}
             alt='Profile photo of Christian Peraza smiling, wearing a white shirt and dark green tie, with a blurred nature background.'
           />
           <p>Christian Peraza</p>
-        </div>
+        </Link>
         <Navigation />
         <FontAwesomeIcon className='burgerMenu-icon fa-lg' icon={faBars} />
       </section>
