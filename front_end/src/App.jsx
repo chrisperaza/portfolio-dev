24d893951358ba1e8542101565c0c7410868b7f2
Christian Peraza <christianperaza2000@gmail.com>
2026-08-07 01:23:48 -0300

    Add scroll to top logic into app

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -5,12 +5,15 @@ import Footer from './components/Footer';
 // Pages
 import Home from './pages/Home';
 import Projects from './pages/Projects';
+// Utils
+import ScrollToTop from './utils/ScrollToTop';
 // Router dom
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
