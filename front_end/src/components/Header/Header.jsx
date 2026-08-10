11bfcd2016e81ff058bccaf641f01e528f136848
Christian Peraza <christianperaza2000@gmail.com>
2026-08-10 20:43:39 -0300

    Create header component folder and reorganize header

--- front_end/src/components/Header/Header.jsx
+++ front_end/src/components/Header/Header.jsx
@@ -1,25 +1,28 @@
-// import React from 'react'
 // Images
-import christianPhoto from '../assets/christianPeraza_photo.png';
+import christianPhoto from '../../assets/christianPeraza_photo.png';
 // Icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 // Components
-import Navigation from './Navigation';
+import Navigation from '../Navigation';
 // React router
 import { Link } from 'react-router-dom';
+// Styles
+import './Header.css';
 
 const Header = () => {
   return (
     <header className='header'>
       <section>
-        <Link to={'/'} title='Home'>
+        <Link to={'/'} title='Home' className='logoLink'>
           <img
             className='profilePhoto-small'
             src={christianPhoto}
             alt='Profile photo of Christian Peraza smiling, wearing a white shirt and dark green tie, with a blurred nature background.'
           />
-          <p>Christian Peraza</p>
+          <p>
+            chris<span>peraza</span>
+          </p>
         </Link>
         <Navigation />
         <FontAwesomeIcon className='burgerMenu-icon fa-lg' icon={faBars} />
