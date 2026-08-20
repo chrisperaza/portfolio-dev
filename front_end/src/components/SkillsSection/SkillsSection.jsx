78210ce710201b221a803b9eae82a0fe9bd04238
Christian Peraza <christianperaza2000@gmail.com>
2026-08-19 23:53:10 -0300

    Improve skills design

--- front_end/src/components/SkillsSection/SkillsSection.jsx
+++ front_end/src/components/SkillsSection/SkillsSection.jsx
@@ -7,14 +7,32 @@ import './SkillsSection.css';
 const SkillsSection = () => {
   return (
     <div className='skillsContainer' id='skills'>
-      <SectionTitles title='My current skills' description='' />
+      <SectionTitles
+        title='Skills and technologies I have mastered'
+        description={
+          <>
+            Take a look at some of the projects I have <b>designed</b> and{' '}
+            <b>developed</b>
+            <span>
+              . These include <b>original</b> ones, some <b>clones</b>, and{' '}
+              <b>landing pages</b>, among others
+            </span>
+          </>
+        }
+      />
       <section className='skillsSection'>
-        <p className='skillsSection-text'>Frontend Development</p>
-        <SkillsList /* itemsQuantity={7} */ type='front' />
-        <p className='skillsSection-text'>Backend & Databases</p>
-        <SkillsList /* itemsQuantity={7} */ type='back-db' />
-        <p className='skillsSection-text'>Tools, Design & Cloud</p>
-        <SkillsList /* itemsQuantity={7} */ type='tools-design-cloud' />
+        <div>
+          <p className='skillsSection-text'>Frontend Development</p>
+          <SkillsList /* itemsQuantity={7} */ type='front' />
+        </div>
+        <div>
+          <p className='skillsSection-text'>Backend & Databases</p>
+          <SkillsList /* itemsQuantity={7} */ type='back-db' />
+        </div>
+        <div>
+          <p className='skillsSection-text'>Tools, Design & Cloud</p>
+          <SkillsList /* itemsQuantity={7} */ type='tools-design-cloud' />
+        </div>
         {/** <button className='skillsSection-button'>
           <p>See more</p>
         </button> **/}
